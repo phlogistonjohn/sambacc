6eb235f95f0a7c7ac21aa8f4067f13518aa9ddc5
John Mulligan <jmulligan@redhat.com>
2026-07-27 13:27:48 -0400

    tests: add unit test coverage for env var mapping
    I ran into a bunch of issues when working on the new env vars for
    SABMACC_{PASSWD,GROUP}_LOCATION. These unit tests add coverage for those
    changes to prevent env var processing regressions.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,6 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 
+from unittest import mock
+
 import pytest
 
 import sambacc.commands.cli
@@ -60,3 +62,126 @@ def test_print_config_env_vars(capsys, tmp_path, monkeypatch):
     assert "path = /share" in out
     assert "[stuff]" in out
     assert "path = /mnt/stuff" in out
+
+
+@pytest.fixture(scope="function")
+def clihack(tmp_path, monkeypatch):
+    mm = mock.MagicMock()
+
+    mm.tcmds = sambacc.commands.cli.CommandBuilder()
+
+    @mm.tcmds.command("check")
+    def _check(ctx):
+        mm.check(ctx)
+
+    mm.fname = tmp_path / "sample.json"
+    with open(mm.fname, "w") as fh:
+        fh.write(config1)
+
+    def _run(args):
+        for key, value in mm.test_env.items():
+            monkeypatch.setenv(key, value)
+        sambacc.commands.main.main_command(mm.tcmds, args)
+
+    mm.run = _run
+    return mm
+
+
+def test_cli_simple(clihack):
+    def _assert(ctx):
+        assert ctx.cli.config == [str(clihack.fname)]
+        assert ctx.cli.identity == "foobar"
+
+    clihack.test_env = {
+        "SAMBACC_CONFIG": str(clihack.fname),
+        "SAMBA_CONTAINER_ID": "foobar",
+    }
+    clihack.check = _assert
+    clihack.run(["check"])
+
+
+def test_cli_opts_simple(clihack):
+    def _assert(ctx):
+        assert ctx.cli.config == [str(clihack.fname)]
+        assert ctx.cli.identity == "foobar"
+
+    clihack.test_env = {}
+    clihack.check = _assert
+    clihack.run(["--identity=foobar", f"--config={clihack.fname}", "check"])
+
+
+def test_cli_altfiles_default(clihack):
+    def _assert(ctx):
+        assert ctx.cli.passwd_location.default_path == "/etc/passwd"
+        assert not ctx.cli.passwd_location.altfiles
+        assert ctx.cli.group_location.default_path == "/etc/group"
+        assert not ctx.cli.group_location.altfiles
+
+    clihack.test_env = {
+        "SAMBACC_CONFIG": str(clihack.fname),
+        "SAMBA_CONTAINER_ID": "foobar",
+    }
+    clihack.check = _assert
+    clihack.run(["check"])
+
+
+def test_cli_altfiles_opts(clihack):
+    def _assert(ctx):
+        assert ctx.cli.passwd_location.default_path == "/etc/passwd"
+        assert ctx.cli.passwd_location.altfiles
+        assert (
+            ctx.cli.passwd_location.mutable_path == "/var/lib/samba/passwd.txt"
+        )
+        assert ctx.cli.passwd_location.link_path == "/lib/passwd"
+
+        assert ctx.cli.group_location.default_path == "/etc/group"
+        assert ctx.cli.group_location.altfiles
+        assert (
+            ctx.cli.group_location.mutable_path == "/var/lib/samba/group.txt"
+        )
+        assert ctx.cli.group_location.link_path == "/lib/group"
+
+    clihack.test_env = {
+        "SAMBACC_CONFIG": str(clihack.fname),
+        "SAMBA_CONTAINER_ID": "foobar",
+    }
+    clihack.check = _assert
+    clihack.run(
+        [
+            "--passwd-location",
+            "/etc/passwd:/var/lib/samba/passwd.txt:/lib/passwd",
+            "--group-location",
+            "/etc/group:/var/lib/samba/group.txt:/lib/group",
+            "check",
+        ]
+    )
+
+
+def test_cli_altfiles_env(clihack):
+    def _assert(ctx):
+        assert ctx.cli.passwd_location.default_path == "/etc/passwd"
+        assert ctx.cli.passwd_location.altfiles
+        assert (
+            ctx.cli.passwd_location.mutable_path == "/var/lib/samba/passwd.txt"
+        )
+        assert ctx.cli.passwd_location.link_path == "/lib/passwd"
+
+        assert ctx.cli.group_location.default_path == "/etc/group"
+        assert ctx.cli.group_location.altfiles
+        assert (
+            ctx.cli.group_location.mutable_path == "/var/lib/samba/group.txt"
+        )
+        assert ctx.cli.group_location.link_path == "/lib/group"
+
+    clihack.test_env = {
+        "SAMBACC_CONFIG": str(clihack.fname),
+        "SAMBA_CONTAINER_ID": "foobar",
+        "SAMBACC_PASSWD_LOCATION": (
+            "/etc/passwd:/var/lib/samba/passwd.txt:/lib/passwd"
+        ),
+        "SAMBACC_GROUP_LOCATION": (
+            "/etc/group:/var/lib/samba/group.txt:/lib/group"
+        ),
+    }
+    clihack.check = _assert
+    clihack.run(["check"])
